5c4cb04708ef52c9a0226e013e372afdf8d49338
EugenioZangara <eugeniozangara@gmail.com>
2023-03-10 16:07:37 -0300

    Clase 5

---
+++ clase5/clase5/Carrito.java
@@ -0,0 +1,17 @@
+package clase5;
+
+public class Carrito {
+
+	public double totalCarrito(double[] subtotales) {
+		double total = 0;
+		for (double subtotal : subtotales) {
+			total = total + subtotal;
+		}
+		return total;
+	}
+	public double descuento(double total, double porcentaje) {
+		return total - (total*porcentaje)/100;
+	}
+	
+	
+}
